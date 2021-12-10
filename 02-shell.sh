5dd38a6a0edd9fa9c9f524cecadf5bb4fe5b6bb5
Gullu <yatham.devops@gmail.com>
2021-12-10 18:58:04 +0530

    initial commit.

--- 02-shell.sh
+++ 02-shell.sh
@@ -8,3 +8,12 @@ echo Welcome
 echo -e "Hello World\nWelcome"
 
 echo -e "word1\tword2"
+
+# Colored output.
+echo -e "\e[31mText in Red Color"
+echo -e "\e[32mText in Green Color"
+echo -e "\e[33mText in Yellow Color"
+echo -e "\e[34mText in Blue Color"
+echo -e "\e[35mText in Magenta Color"
+echo -e "\e[36mText in Cyan Color"
+
