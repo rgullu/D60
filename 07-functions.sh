d10245656b7fc1e77be235a2804f3ff0cb819bc5
Gullu <yatham.devops@gmail.com>
2021-12-11 16:18:55 +0530

    Created abc function.

---
+++ 07-functions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## functions always use before declare.
+## So that is the reason always we see functions at the starting stage.
+function abc(){
+  echo I am a funciton abc
+}
+
+# Calling main funciton
+abc
