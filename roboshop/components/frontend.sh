303793af4e329ee46be4352daadd8d46e02dc58c
Gullu <yatham.devops@gmail.com>
2021-12-12 17:58:12 +0530

    Added -o to repalce the readme file while unzipping.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,14 +5,6 @@ Print "Installing Nginx"
 yum install nginx -y &>>$LOG
 Stat $?
 
-Print "Enabling Nginx"
-systemctl enable nginx &>>$LOG
-Stat $?
-
-Print "Starting Nginx"
-systemctl start nginx &>>$LOG
-Stat $?
-
 Print "Download Html Page"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Stat $?
@@ -26,11 +18,17 @@ unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
 Stat $?
 
 Print "Copy files to Nginx path"
-mv /tmp/frontend-main/static/* /usr/share/nginx/html/.
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
 Stat $?
 
-#mv frontend-main/* .
-#mv static/* .
-#rm -rf frontend-master static README.md
-#mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#systemctl restart nginx
+Print "Copy Nginx Roboshop Config file"
+cp /tmp/frontedn-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+Stat $?
+
+Print "Enabling Nginx"
+systemctl enable nginx &>>$LOG
+Stat $?
+
+Print "Starting Nginx"
+systemctl restart nginx &>>$LOG
+Stat $?
