e6d6109357f1f16f90ae4bb88c93c1d848b9f7f5
Gullu <yatham.devops@gmail.com>
2021-12-12 21:25:34 +0530

    added catalouge code.

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+source components/common.sh
+
+Print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+Stat $?
+
+Print "Add Roboshop User"
+useradd roboshop &>>$LOG
+Stat $?
+
+Print "Download Catalogue"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+Stat $?
+
+Print "Remove Old Content"
+rm -rf /home/roboshop/catalogue &>>$LOG
+Stat $?
+
+Print "Extract Catalogue"
+unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
+Stat $?
+
+
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+#NOTE: We need to update the IP address of MONGODB Server in systemd.service file
+#Now, lets set up the service with systemctl.
+#
+## mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+## systemctl daemon-reload
+## systemctl start catalogue
+## systemctl enable catalogue
