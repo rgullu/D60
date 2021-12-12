695212c4ba38b2f02cc4478a339e17b0cff723c8
Gullu <yatham.devops@gmail.com>
2021-12-12 10:51:03 +0530

    Created another xyz function.1

--- 07-functions.sh
+++ 07-functions.sh
@@ -3,10 +3,14 @@
 ## So that is the reason always we see functions at the starting stage.
 function abc(){
   echo I am a funciton abc
+  echo a in function = $a
+  b=20
 }
 xyz(){
   echo I am a funciton xyz
 }
 # Calling main funciton
+a=10
 abc
+echo b in main = $b
 xyz
