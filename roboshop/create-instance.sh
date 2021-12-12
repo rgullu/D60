769a0f28d102e8476eb03545ecabed17177aaeef
Gullu <yatham.devops@gmail.com>
2021-12-12 20:04:43 +0530

    filtering based on the private id.

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId"|wc -l)
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=cart" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
