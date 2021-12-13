7aefb32f66b4d57360efa8732cae83cbe9d508fb
Gullu <yatham.devops@gmail.com>
2021-12-13 23:29:35 +0530

    added Mline to the all apps.

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -2,22 +2,22 @@
 source components/common.sh
 MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
 
-Print("Install Redis Repos")
+Print "Install Redis Repos"
 yum install yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 Stat $?
 
-Print("Enable Redis Repos")
+Print "Enable Redis Repos"
 yum-config-manager --enable remi &>>$LOG
 Stat $?
 
-Print("Install Redis")
+Print "Install Redis"
 yum install redis -y &>>$LOG
 Stat $?
 
-Print("Update Redis Listen Address")
+Print "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>$LOG
 Stat $?
 
-Print("Start Redis Database")
+Print "Start Redis Database"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Stat $?
