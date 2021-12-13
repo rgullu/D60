daae79c184e13d107417563bf73ddce1c88e4e53
Gullu <yatham.devops@gmail.com>
2021-12-13 16:26:24 +0530

    Changed instance type from t3 to t2.

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exists"
 fi
