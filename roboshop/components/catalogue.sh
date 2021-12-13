daa29250e317fadffefe4952379fb26d3ea7f889
Gullu <yatham.devops@gmail.com>
2021-12-14 00:02:40 +0530

    Update the ip address.

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,43 +2,10 @@
 source components/common.sh
 MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
 
-Print "Installing NodeJS"
-yum install nodejs make gcc-c++ -y &>>$LOG
-Stat $?
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
 
-Print "Add RoboShop User"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-  echo "User RoboShop already exists" &>>$LOG
-else
-  useradd roboshop &>>$LOG
-fi
-Stat $?
-
-Print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-Stat $?
-
-Print "Remove Old Content"
-rm -rf /home/roboshop/catalogue &>>$LOG
-Stat $?
-
-Print "Extract Catalogue"
-unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
-Stat $?
-
-Print "Copy Content"
-mv /home/roboshop/catalogue-main /home/roboshop/catalogue &>>$LOG
-Stat $?
-
-Print "Install NodeJS dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$LOG
-Stat $?
-
-Print "Fix App Permissions"
-chown -R roboshop:roboshop /home/roboshop &>>$LOG
-Stat $?
+NODEJS
 
 Print "Update DNS record in SystemD config"
 sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service &>>$LOG
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -23,3 +23,55 @@ Stat(){
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+NODEJS(){
+  Print "Installing NodeJS"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  Stat $?
+
+  Print "Add RoboShop $COMPONENT_NAME"
+  id roboshop &>>$LOG
+  if [ $? -eq 0 ]; then
+    echo "${COMPONENT_NAME} RoboShop already exists" &>>$LOG
+  else
+    useradd roboshop &>>$LOG
+  fi
+  Stat $?
+
+  Print "Download $COMPONENT_NAME"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  Stat $?
+
+  Print "Remove Old Content"
+  rm -rf /home/roboshop/${COMPONENT} &>>$LOG
+  Stat $?
+
+  Print "Extract ${COMPONENT_NAME}) Content"
+  unzip -o -d /home/roboshop /tmp/${COMPONENT}.zip &>>$LOG
+  Stat $?
+
+  Print "Copy Content"
+  mv /home/roboshop/${COMPONENT}-main /home/roboshop/${COMPONENT} &>>$LOG
+  Stat $?
+
+  Print "Install NodeJS dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install --unsafe-perm &>>$LOG
+  Stat $?
+
+  Print "Fix App Permissions"
+  chown -R roboshop:roboshop /home/roboshop &>>$LOG
+  Stat $?
+
+  Print "Update DNS record in SystemD config"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG
+  Stat $?
+
+  Print "Copy SystemD file"
+  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG
+  Stat $?
+
+  Print "Start Catalogue Service"
+  systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+  Stat $?
+}
