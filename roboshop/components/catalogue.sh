2b337d7fecec5bacc2189acded99027f8e118edc
Gullu <yatham.devops@gmail.com>
2021-12-13 22:30:58 +0530

    added Mline to the all apps.

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source components/common.sh
-
 MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
 
 Print "Installing NodeJS"
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source components/common.sh
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
 
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG
