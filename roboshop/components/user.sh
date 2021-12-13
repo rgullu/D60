7d404e652086bd80db70e7c5c2a169e3fcb3a33c
Gullu <yatham.devops@gmail.com>
2021-12-14 00:15:47 +0530

    Removed duplicate code from the User, Catalogue code.

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source components/common.sh
-MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
+MSPACE=$(cat $0 components/common.sh| grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }'|sort|tail -1)
 
 COMPONENT_NAME=User
 COMPONENT=user
