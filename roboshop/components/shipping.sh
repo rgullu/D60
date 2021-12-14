8a80a8bd783efd005ddb0c67ef4a433938508081
Gullu <yatham.devops@gmail.com>
2021-12-15 00:59:27 +0530

    Adding mysql configuration.

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -15,21 +15,3 @@ if [ "$STAT" == "OK" ] ; then
 else
   Stat 1
 fi
-
-
-#Download the repo
-#$ cd /home/roboshop
-#$ curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
-#$ unzip /tmp/shipping.zip
-#$ mv shipping-main shipping
-#$ cd shipping
-#$ mvn clean package
-#$ mv target/shipping-1.0.jar shipping.jar
-#Update Servers IP address in /home/roboshop/shipping/systemd.service
-#
-#Copy the service file and start the service.
-#
-## mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
-## systemctl daemon-reload
-## systemctl start shipping
-## systemctl enable shipping
