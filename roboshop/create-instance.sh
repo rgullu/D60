d075891acc3bf0601dac32216a0b564ad121d0c2
Gullu <yatham.devops@gmail.com>
2021-12-14 20:16:51 +0530

    Added sleep to the main instance creation code.

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -6,6 +6,8 @@ else
   echo "Instance already exists"
 fi
 
+sleep 5
+
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null|xargs)
 # xargs is used to remove the double quotes for ipaddress. or you can remove xargs here and record.json file IPADDRESS remove the quotes.
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
