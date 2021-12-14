6d5ca0ad3a37be8bcc2c73ea79fb93f6c57fc26b
Gullu <yatham.devops@gmail.com>
2021-12-15 00:43:11 +0530

    Adding mysql configuration.

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -9,7 +9,7 @@ MAVEN
 
 Print "Checking DB Connections from App"
 sleep 15
-STAT=$(curl -s http://localhost:8080/health)
+STAT=$(curl -s https://localhost:8080/health)
 if [ "$STAT" == "OK" ] ; then
   Stat 0
 else
