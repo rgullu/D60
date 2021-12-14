70ed81d9a992b4e3d1d5b597ae0f9a2798339d2a
Gullu <yatham.devops@gmail.com>
2021-12-14 18:54:14 +0530

    Creating Spot instance

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0855cab4944392d0a request-spot-instances --spot-price "0.050" --instance-count 5 --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exists"
 fi
