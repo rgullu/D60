966d1446449e895a718df6da9ee2826e25370345
Gullu <yatham.devops@gmail.com>
2021-12-15 22:49:37 +0530

    Updated the Nginx config file.

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -6,29 +6,29 @@ COMPONENT_NAME=MongoDB
 COMPONENT=mongodb
 
 Print "Download Repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/roboshop-devops-project/${COMPONENT}/main/mongo.repo &>>$LOG
 Stat $?
 
-Print "Install MongoDB"
-yum install -y mongodb-org &>>$LOG
+Print "Install ${COMPONENT_NAME}"
+yum install -y ${COMPONENT}-org &>>$LOG
 Stat $?
 
-Print "Update MongoDB Config"
+Print "Update ${COMPONENT_NAME} Config"
 sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>$LOG
 Stat $?
 
-Print "Starting MongoDB"
+Print "Starting ${COMPONENT_NAME}"
 systemctl restart mongod &>>$LOG
 Stat $?
 
-Print "Enabling MongoDB"
+Print "Enabling ${COMPONENT_NAME}"
 systemctl enable mongod &>>$LOG
 Stat $?
 
 DOWNLOAD "/tmp"
 
 Print "Load Schema"
-cd /tmp/mongodb-main
+cd /tmp/${COMPONENT}-main
 for db in catalogue users; do
   mongo < $db.js &>>$LOG
 done
