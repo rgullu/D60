1b0df89693892f77aecce4d0ab460ca8bfecedbb
Gullu <yatham.devops@gmail.com>
2021-12-15 22:57:46 +0530

    Updated the Nginx config file.

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ "$1" == "all" ]; then
+  ALL=(frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment)
+  for component in ${ALL[*]};do
+    echo "Creating Instance - $component"
+  done
+fi
+exit
+
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
