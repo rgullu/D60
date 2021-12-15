d28d8bbda4e28eb13798537bcc92c08292d176fb
Gullu <yatham.devops@gmail.com>
2021-12-15 15:56:26 +0530

    Just Checking the Stat Value

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -6,8 +6,13 @@ COMPONENT_NAME=Shipping
 COMPONENT=shipping
 
 Print "Install Erlang"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
-Stat $?
+yum list installed | grep "erlang" &>>$LOG
+if [ $? -ne 0 ] ; then
+  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
+else
+  Stat $?
+fi
+
 Print "Setup YUM repositories for RabbitMQ."
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
 Stat $?
