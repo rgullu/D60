7d560e818331554846d8db62108babbf21f8c74f
Gullu <yatham.devops@gmail.com>
2021-12-15 23:14:36 +0530

    Updated the Nginx config file.

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -3,7 +3,7 @@ CREATE(){
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t2.micro --security-group-ids sg-06a565a11eb44c7bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo "$1 Instance already exists"
   fi
