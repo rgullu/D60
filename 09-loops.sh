2f7bc9001ef888ca3ead898472e514d3f0d5724f
Gullu <yatham.devops@gmail.com>
2021-12-16 11:27:21 +0530

    Updated the Nginx config file.

--- 09-loops.sh
+++ 09-loops.sh
@@ -3,26 +3,25 @@
 #Loops are two major commands, while & For.
 # While loop works on Expressions that we used in if statements.
 
-a=10
-while [ $a -gt 0 ]; do
-  echo While Loops
-  sleep 0.5
-  a=$(($a-1))
-  done
-echo "-------------------------------"
-# For loop example.
-for fruit in apple banana orange peach ; do
-  echo Fruit Name = $fruit
-done
+#a=10
+#while [ $a -gt 0 ]; do
+#  echo While Loops
+#  sleep 0.5
+#  a=$(($a-1))
+#  done
+#echo "-------------------------------"
+## For loop example.
+#for fruit in apple banana orange peach ; do
+#  echo Fruit Name = $fruit
+#done
 
 # While loop example.
 echo -n "Checking connection on port 22 for Host $1"
 while true; do
-  nc -z $1 22 &>/dev/null
+  nc -w 1 -z $1 22 &>/dev/null
   if [ $? -eq 0 ]; then
     break
   fi
-  sleep 1
   echo -n "."
 done
 
