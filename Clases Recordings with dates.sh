ab838406f87fc2e0e5dfa32c67d1c8980c5c4acb
Gullu <yatham.devops@gmail.com>
2021-12-16 11:38:06 +0530

    Updated the Nginx config file.

---
+++ Clases Recordings with dates.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+Main sessions will starts from here
+09-Oct-21: Yum, Permissions, Chown, chgrp explanation.
+11-Oct-21: Troubleshooting tips, Creating Securtiy grops, How web applications works.
+12-Oct-21: How webserver works, Forward proxy, Reverse proxy, Advantages of micro services.
+13-Oct-21: Project setup with manual process.
+14-Oct-21: Project setup with manual process.
+18-Oct-21: Pain points while exectuting project via manual effort.
+---------------
+youtube.com/watch?v=bw8QXw4rpcc #XShell, Intellij installaiton video.
+---------------
+19-Oct-21: IntelliJ installation, connecting to Github via Intellij and Shell Script Basics.
+20-Oct-21: Shell Scripting(Variables, echo, Colors).
+21-Oct-21: Redirectors, Exit Status & Functions.
+22-Oct-21: Implementing Shell Scripting for Frontend component.
+23-Oct-21: Frontend component continution and AWS(CLI)Creating instances using Shell scripting, Creating MongoDB componet, Crating Catalogue component.
+25-Oct-21: Continuetion of Catalouge component, crating route53 and creating DNS and updating the IP address of Mongodb in serviced under catalogue, configure redis,
+	   Configure User,Configure Cart (This video is very Important)
+26-Oct-21: Remaining Components Configurations. Applicaiton is working after deploye all the modules.
+27-Oct-21: Create Instance file enhancment and While, For loops explanation,
+
+
+
+27-OCT-21: Ansible installation
+28-OCT-21: Ansible basics
+29-OCT-21: Project setup with Ansible
+30-OCT-21: Project setup with Ansible
+01-Nov-21: Project setup with Ansible
+02-Nov-21: Project setup with Ansible
+Terraform:-
+03-Nov-21: Terraform
+04-Nov-21: Terraform
+08-Nov-21: Terraform
+09-Nov-21: Terraform
+10-Nov-21: Load Balancer, VPC, Estimate Subnets ip address.
+11-Nov-21: Creating VPC using terraform and Estimate IP address.
+
+15-Nov-21: Mutable project imlementation with Terraform
+16-Nov-21: Mutable project imlementation with Terraform
+17-Nov-21: Mutable project imlementation with Terraform
+18-Nov-21: Mutable project imlementation with Terraform
+19-Nov-21: Mutable project imlementation with Terraform
+20-Nov-21: Mutable project imlementation with Terraform
+22-Nov-21: Mutable project imlementation with Terraform
+23-Nov-21: Mutable project imlementation with Terraform
+
+24-Nov-21: CICD/ pipeline code explanation, like stage, trigger, environment, automate the jenkins GUI via script.
+25-Nov-21: Tags,
+27-Nov-21: Multi branching, Code Quality checks using SonarQube tool.
+29-Nov-21: Creating Artifacts by using Nexus.
+30-Nov-21: Creating pipeline for Mutable infrastructure.
+01-Dec-21: Creating FQDN in Hostinger and setup the PROD environment like Dev.
+02-Dec-21: Promothous demo, email alert.(Monitoring tool).
+03-Dec-21: Monitoring tool demo continution.
+04-Dec-21: Elastic Searching, file beast
+06-Dec-21: Log management
+07-Dec-21: Log management.
+08-Dec-21: Creating Grafts on top of logs using Elastic Searching visuvilization.
+
+15-Dec-21: Immutable Approach final.
+
+
+
+
+
+
+
+
