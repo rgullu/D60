45c98d5074c2bd343e8287d1e5e147d4ce019b46
Gullu <yatham.devops@gmail.com>
2021-12-27 22:30:03 +0530

    added shell spot code and record.json file into the ansible directory.

---
+++ ansible/create-spot-instance.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+CREATE(){
+  COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
+
+  if [ $COUNT -eq 0 ]; then
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-04fa5b9acf459ac11,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+  else
+    echo -e "\e[1;33m$1 Instance already exists\e[0m"
+    return
+  fi
+
+  sleep 5
+
+  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null|xargs)
+  # xargs is used to remove the double quotes for ipaddress. or you can remove xargs here and record.json file IPADDRESS remove the quotes.
+  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+
+  aws route53 change-resource-record-sets --hosted-zone-id Z08824272G1CUC58UUD82 --change-batch file:///tmp/record.json | jq &>/dev/null
+}
+
+if [ "$1" == "all" ]; then
+  ALL=(frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment)
+  for component in ${ALL[*]};do
+    echo "Creating Instance - $component"
+    CREATE $component
+  done
+else
+  echo "Creating Instance - $1"
+  CREATE $1
+fi
