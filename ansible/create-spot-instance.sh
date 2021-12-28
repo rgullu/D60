cea8a7a2db0c2d4761ff6117500916626abd341f
Gullu <yatham.devops@gmail.com>
2021-12-28 20:59:32 +0530

    working on frontend copying files from remote.

--- ansible/create-spot-instance.sh
+++ ansible/create-spot-instance.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+UPDATE_DNS_RECORDS(){
+    IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null|xargs)
+    # xargs is used to remove the double quotes for ipaddress. or you can remove xargs here and record.json file IPADDRESS remove the quotes.
+    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z08824272G1CUC58UUD82 --change-batch file:///tmp/record.json | jq &>/dev/null
+}
+
 CREATE(){
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress"|grep -v null|wc -l)
 
@@ -6,16 +14,13 @@ CREATE(){
     aws ec2 run-instances --launch-template LaunchTemplateId=lt-04fa5b9acf459ac11,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
+    UPDATE_DNS_RECORDS $1
     return
   fi
 
   sleep 5
 
-  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null|xargs)
-  # xargs is used to remove the double quotes for ipaddress. or you can remove xargs here and record.json file IPADDRESS remove the quotes.
-  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-
-  aws route53 change-resource-record-sets --hosted-zone-id Z08824272G1CUC58UUD82 --change-batch file:///tmp/record.json | jq &>/dev/null
+  UPDATE_DNS_RECORDS $1
 }
 
 if [ "$1" == "all" ]; then
